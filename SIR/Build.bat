85a5e0b680fec772d567c84b6e9e0ba68ffa4600
Sagar Donadkar <sp.donadkar@gmail.com>
2022-10-05 19:48:52 +0530

    final demo with light

--- SIR/Build.bat
+++ SIR/Build.bat
@@ -1,8 +1,8 @@
 cls
-del PerFragment.exe
-del PerFragment.obj
-del PerFragment.res
+del BirthdayDemo.exe
+del BirthdayDemo.obj
+del BirthdayDemo.res
 del log.txt
-cl.exe /c /EHsc /I C:\glew-2.1.0-win32\glew-2.1.0\include PerFragment.cpp
-rc.exe PerFragment.rc
-link.exe PerFragment.obj PerFragment.res /LIBPATH:C:\glew-2.1.0-win32\glew-2.1.0\lib\Release\x64 user32.lib Sphere.lib gdi32.lib
+cl.exe /c /EHsc /I C:\glew-2.1.0-win32\glew-2.1.0\include BirthdayDemo.cpp
+rc.exe BirthdayDemo.rc
+link.exe BirthdayDemo.obj BirthdayDemo.res /LIBPATH:C:\glew-2.1.0-win32\glew-2.1.0\lib\Release\x64 user32.lib Sphere.lib gdi32.lib
